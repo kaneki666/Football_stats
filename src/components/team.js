ea6111706ea91e8031fef648e1cfce3ce0b930d8
kaneki666 <41444793+kaneki666@users.noreply.github.com>
2019-12-08 17:31:48 +0600

    commit change

--- src/components/team.js
+++ src/components/team.js
@@ -1,18 +1,25 @@
 import  React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
   },
 }));
+
 
 const Team = () => {
 
@@ -34,33 +41,27 @@ const Team = () => {
     const classes = useStyles();
     
     return(
-        <div>
-            {datas.map(data => (
-                <div className={classes.root}>
-                <GridList cellHeight={180} className={classes.gridList}>
-                  <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-                    <ListSubheader component="div">December</ListSubheader>
-                  </GridListTile>
-                  {tileData.map(tile => (
-                    <GridListTile key={data.img}>
-                      <img src={tile.img} alt={tile.title} />
-                      <GridListTileBar
-                        title={tile.title}
-                        subtitle={<span>by: {tile.author}</span>}
-                        actionIcon={
-                          <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
-                            <InfoIcon />
-                          </IconButton>
-                        }
-                      />
-                    </GridListTile>
-                  ))}
-                </GridList>
-              </div>
-              </div>
-            ))}
-        </div>
-    )
+    <div className={classes.root}>
+      <GridList cellHeight={180} className={classes.gridList}>
+        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+          <ListSubheader component="div"></ListSubheader>
+        </GridListTile>
+        {datas.map(data => (
+          <GridListTile key={data.id}>
+            <img src={data.image_path} alt={data.name} />
+            <GridListTileBar
+              title={data.name}
+              actionIcon={
+                <IconButton aria-label={`info about `} className={classes.icon}>
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  )
 }
 
 export default Team;
